de08ffcb7a521ed3cb5b16c60900d6b1dd1245e7
ian <barkatthetree@gmail.com>
2015-12-12 17:27:04 +0800

    changed gitignore

--- forge/src/main/java/pregnantboy/tutorial/Reference.java
+++
@@ -1,9 +0,0 @@
-package pregnantboy.tutorial;
-
-public class Reference {
-	public static final String MOD_ID = "pregnantboy";
-	public static final String MOD_NAME = "Tutorial Mod";
-	public static final String VERSION = "1.0";
-	public static final String CLIENT_PROXY_CLASS = "pregnantboy.tutorial.proxy.ClientProxy";
-	public static final String SERVER_PROXY_CLASS = "pregnantboy.tutorial.proxy.ServerProxy";
-}
--- forge/src/main/java/pregnantboy/tutorial/proxy/ServerProxy.java
+++
@@ -1,6 +0,0 @@
-package pregnantboy.tutorial.proxy;
-
-public class ServerProxy {
-	public void registerRenders() {
-	}
-}
